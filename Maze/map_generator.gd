ce31b794d0142315b43b3ac61d628b7ee46e4261
JoelCoyos <coyos52@gmail.com>
2025-01-25 13:09:45 -0300

    maze generation

---
+++ Maze/map_generator.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+
+var mazeBlockCommon
+var mazeBlockUp
+var mazeBlockMiddle
+var mazeBlockDown
+
+var direction
+var lastCrossPosition
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	mazeBlockCommon = load("res://MapBlockCommon.tscn")
+	mazeBlockUp = load("res://MapBlockUp.tscn")
+	mazeBlockMiddle = load("res://MapBlockMiddle.tscn")
+	mazeBlockDown = load("res://MapBlockDown.tscn")
+	var blockInstance = mazeBlockCommon.instantiate()
+	add_child(blockInstance)
+	direction = 0
+	lastCrossPosition = Vector2(0.0,0.0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("SpawnUp"):
+		direction = 1
+		SpawnNewBlock()
+	
+	if Input.is_action_just_pressed("SpawnMiddle"):
+		direction = 0
+		SpawnNewBlock()
+		
+	if Input.is_action_just_pressed("SpawnDown"):
+		direction = -1
+		SpawnNewBlock()	
+	pass
+	
+func SpawnNewBlock():
+	var blockInstance
+	if direction == 1:
+		lastCrossPosition = Vector2(lastCrossPosition.x+ 352.0,lastCrossPosition.y +  -64.0)
+		blockInstance = mazeBlockUp.instantiate()
+	elif direction ==0:
+		lastCrossPosition = Vector2(lastCrossPosition.x+ 352.0,lastCrossPosition.y + 0.0)
+		blockInstance = mazeBlockMiddle.instantiate()
+	elif direction ==-1:
+		lastCrossPosition = Vector2(lastCrossPosition.x+ 352.0,lastCrossPosition.y + 64.0)
+		blockInstance = mazeBlockDown.instantiate()
+		
+
+	add_child(blockInstance)
+	blockInstance.position = lastCrossPosition
+	$Camera.position = lastCrossPosition
+		
+		
+	
