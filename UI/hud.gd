1025b9ca4279ed5dff1bd5f3e5da99bbccd77fd3
Lautaro Di Bartolo <lautarodibartolo@gmail.com>
2025-01-26 10:45:16 -0300

    feat: Life bar

--- UI/hud.gd
+++ UI/hud.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+@export var heart_image : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,12 @@ func _process(delta: float) -> void:
 	pass
 	
 func update_life(life: int) -> void:
-	$Control/Life.text = str(life)
+	for child in $LifeBar.get_children():
+		child.queue_free()
+		
+	for i in range(life):
+		var heart = heart_image.instantiate()
+		$LifeBar.add_child(heart)
 
 func update_mana(mana: int) -> void:
 	$Control/Mana.text = str(mana)
