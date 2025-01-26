9102dc012a96658eafb72f0228b8ef8e40efcac2
valentin <nogarvalentin9@gmail.com>
2025-01-26 17:03:57 -0300

    MAQUETA FINAL
    MAQUETA FINAL

--- Victory/victory.gd
+++ Victory/victory.gd
@@ -2,5 +2,10 @@ extends Control
 
 var menu_scene = load("res://Menu/menu.tscn")
 
+func _process(delta: float) -> void:
+
+	if Input.is_action_just_pressed("mana"):
+		get_tree().change_scene_to_packed(menu_scene)
+
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(menu_scene)
