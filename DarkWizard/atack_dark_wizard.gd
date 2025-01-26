5ef2abbe78894ee243c3fc21d42acd4fac31afb5
Lautaro Di Bartolo <lautarodibartolo@gmail.com>
2025-01-26 13:50:40 -0300

    feat: Dark Wizard One Shot

--- DarkWizard/atack_dark_wizard.gd
+++ DarkWizard/atack_dark_wizard.gd
@@ -1,16 +1,25 @@
 extends Area2D
 
+signal arrow_hit
+
 @export var speed : int
+var is_attacking: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$CollisionShape2D.disabled = true
+	is_attacking = false
+	$Sprite2D.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Mueve el objeto hacia la izquierda
-	global_position.x -= speed * delta
+	if is_attacking:
+		# Mueve el objeto hacia la izquierda
+		global_position.x -= speed * delta
+
+func attack() -> void:
+	is_attacking = true
+	$Sprite2D.visible = true
 
 
-func _on_timer_timeout() -> void:
-	$CollisionShape2D.disabled = false
+func _on_tree_exited() -> void:
+	arrow_hit.emit()
--- DarkWizard/darkwizard.gd
+++ DarkWizard/darkwizard.gd
@@ -4,14 +4,15 @@ extends Area2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var atack : PackedScene
-
-
-func _ready() -> void:
-	var arrow = atack.instantiate()
-	add_child(arrow)
+var attacked = false
 
 func _on_area_entered(area: Area2D) -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 0, 0.1)
-	await tween.finished
+	if not attacked:
+		attacked = true
+		$ArrowDarkWizard.attack()
+		var tween = create_tween()
+		tween.tween_property($Sprite2D, "modulate:a", 0, 0.1)
+
+
+func _on_arrow_dark_wizard_arrow_hit() -> void:
 	queue_free()
